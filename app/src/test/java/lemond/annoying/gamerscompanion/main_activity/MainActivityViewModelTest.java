95439f5def630423c44716e1f1efe68dedb954b1
Kurt Lemond <kurt.lemond@icemobile.com>
2018-03-07 20:18:06 -0500

    Added page change test on MainActivityTest

--- app/src/test/java/lemond/annoying/gamerscompanion/main_activity/MainActivityViewModelTest.java
+++ app/src/test/java/lemond/annoying/gamerscompanion/main_activity/MainActivityViewModelTest.java
@@ -1,15 +1,24 @@
 package lemond.annoying.gamerscompanion.main_activity;
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule;
+import android.arch.lifecycle.Observer;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import lemond.annoying.gamerscompanion.activity.viewmodel.MainActivityViewModel;
 
+@RunWith(MockitoJUnitRunner.class)
 public class MainActivityViewModelTest {
 
     private MainActivityViewModel mainActivityViewModel;
@@ -17,9 +26,18 @@ public class MainActivityViewModelTest {
     @Rule
     public TestRule rule = new InstantTaskExecutorRule();
 
+    @Mock
+    private Observer<Integer> pageChangeObserver;
+
     @Before
-    public void setUp() {
+    public void start() {
         mainActivityViewModel = new MainActivityViewModel();
+        mainActivityViewModel.getSelectedPageLiveData().observeForever(pageChangeObserver);
+    }
+
+    @After
+    public void finish() {
+        mainActivityViewModel.getSelectedPageLiveData().removeObserver(pageChangeObserver);
     }
 
     @Test
@@ -28,6 +46,7 @@ public class MainActivityViewModelTest {
         mainActivityViewModel.setSelectedPage(selectedPage);
 
         assertNotNull(mainActivityViewModel.getSelectedPageLiveData().getValue());
+        verify(pageChangeObserver).onChanged(2);
         assertTrue(mainActivityViewModel.getSelectedPageLiveData().getValue() == 2);
     }
 
@@ -38,12 +57,14 @@ public class MainActivityViewModelTest {
 
         assertNotNull(mainActivityViewModel.getSelectedPageLiveData().getValue());
         assertTrue(mainActivityViewModel.getSelectedPageLiveData().getValue() == 2);
+        verify(pageChangeObserver).onChanged(2);
 
         selectedPage = 0;
         mainActivityViewModel.setSelectedPage(selectedPage);
         assertNotNull(mainActivityViewModel.getSelectedPageLiveData().getValue());
         assertTrue(mainActivityViewModel.getSelectedPageLiveData().getValue() == 0);
         assertFalse(mainActivityViewModel.shouldAnimateScrollToTop());
+        verify(pageChangeObserver, times(2)).onChanged(0);
     }
 
     @Test
@@ -53,12 +74,14 @@ public class MainActivityViewModelTest {
 
         assertNotNull(mainActivityViewModel.getSelectedPageLiveData().getValue());
         assertTrue(mainActivityViewModel.getSelectedPageLiveData().getValue() == 2);
+        verify(pageChangeObserver).onChanged(2);
 
         selectedPage = 2;
         mainActivityViewModel.setSelectedPage(selectedPage);
         assertNotNull(mainActivityViewModel.getSelectedPageLiveData().getValue());
         assertTrue(mainActivityViewModel.getSelectedPageLiveData().getValue() == 2);
         assertTrue(mainActivityViewModel.shouldAnimateScrollToTop());
+        verify(pageChangeObserver, times(2)).onChanged(2);
     }
 
 }
